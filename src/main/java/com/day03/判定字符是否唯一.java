f6b69d1ef9464ec2b3cc1e3ce89fcaebf63fc196
jzl <793405922@qq.com>
2020-07-10 09:43:57 +0800

    LeetCode 第三天打卡题 判定字符是否唯一 使用位运算存储状态

---
+++ src/main/java/com/day03/判定字符是否唯一.java
@@ -0,0 +1,31 @@
+package com.day03;
+
+/**
+ * https://leetcode-cn.com/problems/is-unique-lcci/
+ */
+public class 判定字符是否唯一 {
+}
+
+class Solution4 {
+    public boolean isUnique(String astr) {
+        long x1 = 0L,x2 = 0L, temp;
+        for(int i = 0;i<astr.length();i++){
+            char c = astr.charAt(i);
+            temp = 1L;
+            if(c >= 64){
+                temp <<= (c-64);
+                if((x2 & temp) == temp){
+                    return false;
+                }
+                x2 |= temp;
+            }else{
+                temp <<= c;
+                if((x1 & temp) == temp){
+                    return false;
+                }
+                x1 |= temp;
+            }
+        }
+        return true;
+    }
+}
