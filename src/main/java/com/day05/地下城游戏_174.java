03a2097a15a32c51d9286b82bdb36ec6a3cbc78d
jzl <793405922@qq.com>
2020-07-12 12:10:25 +0800

    LeetCode 第五天打卡题 简单反向dp 20.7.12

---
+++ src/main/java/com/day05/地下城游戏_174.java
@@ -0,0 +1,26 @@
+package com.day05;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode-cn.com/problems/dungeon-game/solution/
+ */
+public class 地下城游戏_174 {
+}
+
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int row = dungeon.length;
+        int col = dungeon[0].length;
+        int[][] dp = new int[row+1][col+1];
+        for(int i = 0;i<=row;i++) Arrays.fill(dp[i],Integer.MAX_VALUE);
+        dp[row][col-1] = dp[row-1][col] = 1;
+        for(int i = row-1;i>=0;i--){
+            for(int j = col-1;j>=0;j--){
+                int minn = Math.min(dp[i][j+1],dp[i+1][j]);
+                dp[i][j] = Math.max(minn-dungeon[i][j],1);
+            }
+        }
+        return dp[0][0];
+    }
+}
