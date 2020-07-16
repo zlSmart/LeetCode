109a10a12a006a51b30d746f173b5cdd7231a472
jzl <793405922@qq.com>
2020-07-16 08:53:49 +0800

    LeetCode 打卡

---
+++ src/main/java/com/day08/从头到尾打印链表.java
@@ -0,0 +1,31 @@
+package com.day08;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+ */
+public class 从头到尾打印链表 {
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
+class Solution4 {
+    public int[] reversePrint(ListNode head) {
+        Stack<Integer> stack= new Stack<>() ;
+        while(head!=null){
+            stack.push(head.val);
+            head = head.next;
+        }
+        int[] result = new int[stack.size()];
+        int i = 0;
+        while(!stack.empty()){
+            result[i++] = stack.pop();
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/day09/翻转二叉树_226.java
@@ -0,0 +1,27 @@
+package com.day09;
+
+/**
+ * https://leetcode-cn.com/problems/invert-binary-tree/
+ */
+public class 翻转二叉树_226 {
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
+class Solution4 {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode temp =  new TreeNode(0);
+        temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
