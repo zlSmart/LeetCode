313fbc890bb3b2405f43932e316b640da0b84ad9
jzl <793405922@qq.com>
2020-08-25 15:39:00 +0800

    LeetCode

---
+++ src/main/java/com/weeks/week2/压缩算法.java
@@ -0,0 +1,26 @@
+package com.weeks.week2;
+
+import java.util.Collections;
+import java.util.Scanner;
+
+public class 压缩算法 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+        scanner.close();
+        System.out.println(decode(next));
+    }
+
+    public static String decode(String words){
+        while (words.contains("]")){
+            int right = words.indexOf("]");
+            int left = words.lastIndexOf("[", right);
+            String repeatStr = words.substring(left+1, right);
+            String[] split = repeatStr.split("\\|");
+            words = words.replace("["+repeatStr+"]",
+                    String.join("", Collections.nCopies(Integer.parseInt(split[0]), split[1])));
+        }
+        return words;
+    }
+}
